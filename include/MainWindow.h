42f25c025da1b2cca2899b403de5265c8fbe71c7
AbyssDragonModding <124967094+AbyssDragonModding@users.noreply.github.com>
2024-12-17 10:55:18 -0700

    Create MainWindow.h

---
+++ include/MainWindow.h
@@ -0,0 +1,14 @@
+#include <imgui.h>
+#pragma once
+
+class MainWindow
+{
+    public:
+        void RenderWindow();
+
+    // Variables
+    private:
+        bool m_TestBool{ false };
+        float m_TestFloat{ 0.25f };
+
+};
